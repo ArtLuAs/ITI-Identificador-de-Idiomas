3c98053c49551c7daff0fd2f1622b7ce88669a11
Maximusthr <arthurmcb321@gmail.com>
2026-03-29 00:22:22 -0300

    refactor: sh

--- run.sh
+++ run.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# 1. Executa o script Python na pasta tools
-echo "--- Rodando o script Python ---"
-python3 tools/script.py
+# 1. Executa o main Python na pasta tools
+echo "--- Rodando o main Python ---"
+python3 tools/main.py
 
 # Se você também tiver o main.py na raiz para rodar, basta adicionar a linha abaixo:
 # python3 main.py
 
 echo -e "\n--- Compilando o código C++ ---"
 # 2. Compila o main.cpp e gera um executável chamado 'programa_cpp' dentro da pasta interpretador
-g++ interpretador/main.cpp -o interpretador/programa_cpp
+g++ main.cpp ./src/*.cpp -o interpretador
 
 # 3. Verifica se a compilação foi bem-sucedida (código de saída 0 significa sucesso)
 if [ $? -eq 0 ]; then
     echo "Compilação concluída com sucesso!"
     echo -e "\n--- Executando o programa C++ ---"
-    
+
+    # Idioma
+    idioma = "pt"
+
     # Executa o programa gerado
-    ./interpretador/programa_cpp
+    ./main --treinar "corpora/${idioma}.txt" "modelos/${idioma}.model" 4
 else
     # Se houver erro de sintaxe no C++, o script avisa e não tenta rodar
     echo "Erro na compilação do C++. A execução foi abortada."
